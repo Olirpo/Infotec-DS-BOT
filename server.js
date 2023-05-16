608d36363731f82752be422fe9bb966c2ded6eae
Joaquin <joaquinfontana11@gmail.com>
2023-05-16 17:18:54 -0300

    Fixed bugs in bot responses

--- server.js
+++ server.js
@@ -1,83 +1,86 @@
-
 /////////CONSTANTES/////////////
-const {fetchUserHours, setUserHours} = require('./utils.js')
+const { fetchUserHours, setUserHours } = require("./utils.js");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require ("./config.json");
+const config = require("./config.json");
 const log = console.log;
 const prefix = config.prefix;
 const alumnos = {
-    Olirpo: 'Oliverio',
-    NEUBLED: 'Martin',
-    DRAGÓN: 'Larraya',
-}
+  Olirpo: "Oliverio",
+  NEUBLED: "Martin",
+  DRAGÓN: "Larraya",
+};
 /////////FUNCION DE ESTADO DE DISCORD////////////////
-const presencia = () =>{
-    client.user.setPresence({
-        status:"online",
-        activity:{
-            name:"twitch.tv/olirpos",
-            type:"WATCHING"
-        }
-    })
-}
+const presencia = () => {
+  client.user.setPresence({
+    status: "online",
+    activity: {
+      name: "twitch.tv/olirpos",
+      type: "WATCHING",
+    },
+  });
+};
 ////////// FUNCION AL ESTAR READY///////////
-client.on("ready", ()=> {
-    console.log(`ready on ${client.user.tag}`);
-    // presencia();
-
-    }
-    
-);
-/////////////////////FUNCION AL RECIBIR MENSAJE/////////////
-client.on("message", async (message)=> {
-    const author = message.author
-    const input = message.content;
-    const command = input.toLocaleLowerCase().split(' ')[0];
-    const args = input.toLocaleLowerCase().split(' ').slice(1);
-
- //Si el autor es un bot no retornar nada
-    if (author.bot){
-    return
-    };  
-//Si no comienza con el prefijo no retornar nada
-    if (!input.startsWith(prefix)){
-        return
-    };
-    ////////////////////////COMANDO AGREGADOS/////////////////////////////////////////////
-    if(command === "!horas"){
-        if(args){
-            devolverMensaje(await fetchUserHours(args[1]), message)
-        } else{
-            alumnos[author.username]
-            ? devolverMensaje(await fetchUserHours(alumnos[author.username]), message)
-            : devolverMensaje('Profeee', message);
-        }
-    }
-    if(command === "!add"){
-        console.log(author.username !== 'Olirpo')
-        if(author.username !== 'K100' && author.username !== 'Joacko' && author.username !== 'Olirpo'){
-            devolverMensaje('Vos no podes añadirte horas panflin', message);
-            return
-        }
-        const hours = args[0]
-        const userToChange = args[1]
-        try{
-            const response = await setUserHours(userToChange, hours);
-            console.log(response)
-            devolverMensaje(`Añadiste correctamente ${hours} horas a ${userToChange}. Nuevas horas totales: ${await fetchUserHours(userToChange)}`,message)
-        } catch (err){
-            console.error(err)
-            devolverMensaje('Algo salió mal', message)
-        }
-        
-    }
-
-    else{
-        devolverMensaje('Que decis flaco', message)
-    }
+client.on("ready", () => {
+  console.log(`ready on ${client.user.tag}`);
+  // presencia();
 });
-    function devolverMensaje(respuesta,message){
-        message.channel.send(respuesta);
+/////////////////////FUNCION AL RECIBIR MENSAJE/////////////
+client.on("message", async (message) => {
+  const author = message.author;
+  const input = message.content;
+  const command = input.toLocaleLowerCase().split(" ")[0];
+  const args = input.toLocaleLowerCase().split(" ").slice(1);
+
+  //Si el autor es un bot no retornar nada
+  if (author.bot) {
+    return;
+  }
+  //Si no comienza con el prefijo no retornar nada
+  if (!input.startsWith(prefix)) {
+    return;
+  }
+  ////////////////////////COMANDO AGREGADOS/////////////////////////////////////////////
+  else if (command === "!horas") {
+    if (args.length !== 0) {
+      devolverMensaje(await fetchUserHours(args[0]), message);
+    } else {
+      alumnos[author.username]
+        ? devolverMensaje(
+            await fetchUserHours(alumnos[author.username]),
+            message
+          )
+        : devolverMensaje("Profeee", message);
     }
+  } else if (command === "!add") {
+    if (
+      author.username !== "K100" &&
+      author.username !== "Joacko" &&
+      author.username !== "Olirpo"
+    ) {
+      devolverMensaje("Vos no podes añadirte horas panflin", message);
+      return;
+    }
+    const hours = args[0];
+    const userToChange = args[1];
+    try {
+      const response = await setUserHours(userToChange, hours);
+      console.log(response);
+      devolverMensaje(
+        `Añadiste correctamente ${hours} horas a ${userToChange}. Nuevas horas totales: ${await fetchUserHours(
+          userToChange
+        )}`,
+        message
+      );
+    } catch (err) {
+      console.error(err);
+      devolverMensaje("Algo salió mal", message);
+    }
+  } else {
+    devolverMensaje("Que decis flaco", message);
+  }
+});
+function devolverMensaje(respuesta, message) {
+  message.channel.send(respuesta);
+}
 client.login(config.token);
--- utils.js
+++ utils.js
@@ -1,137 +1,130 @@
-const {google} = require('googleapis');
+const { google } = require("googleapis");
 //const spreadsheetId = '1im_YNMczWd2bqw8m0ueOQQ1gFpHwnDK74cRGBPEKeVY'; // OLD ID
-const spreadsheetId = '1SgiHV19wxoe18emjUn8xRqwDLFYukp8-V3X0LJFR_oM'; //NEW ID
+const spreadsheetId = "14BAQXJdSQQDRVch_AxvKvGl7I9a-rHcwvCSH6YLnwes"; // NEW ID
 const log = console.log;
 const USER_COLUMNS = {
-    'Fecha/Alumno': 'A3:A',
-    'Maximo Larraya': 'B3:B',
-    'Martin Delbueno': 'C3:C',
-    'Oliverio Velazquez': 'D3:D',
-}
-const setUserHours = async (user, hours, date = today())=>{
-    console.log({user,hours,date})
-    let {requestedUserColumn, arrOfColumns} = await getUserColumns(user)
-    
-    const indexOfNewCell = requestedUserColumn.findIndex(e=> e==='')
-    const userColumnIndex = getIndexColumn(arrOfColumns,requestedUserColumn)
-    requestedUserColumn[indexOfNewCell] = hours;
-    arrOfColumns[userColumnIndex] = requestedUserColumn;
-
-    const dateColumns = arrOfColumns[0]
-    if(!dateColumns[indexOfNewCell]){
-        dateColumns[indexOfNewCell] = date
-        arrOfColumns[0] = dateColumns
-    }
-    let res = null;
-    try{
-        res = await setSheetInfo('COLUMNS', 'InfoTEC!A3:D102', arrOfColumns);
-        console.log(res)
-        }    catch (error){
-            console.error(error)
-        }
-        finally{
-            return res.data.totalUpdatedColumns
-        }
-}
-
-const fetchUserHours = async(user)=>{ 
-
-
-    const {requestedUserColumn} = await getUserColumns(user)
-
-    const totalHours = requestedUserColumn.at(-1)
-
-    return totalHours
-}
-
-const googleAuth = ()=>{
-    return new google.auth.GoogleAuth({
-        keyFile: './credentials.json',
-        scopes: 'https://www.googleapis.com/auth/spreadsheets'
-    });
-}
-const fetchSheets = async()=>{
-    const auth = googleAuth();
-    const client = await auth.getClient();
-    const sheets = google.sheets({version:'v4', auth: client})
-    return {
-        sheets,
-        auth
-    }
-}
-const getSheetInfo = async (format, range='InfoTEC!A3:D102')=>{
-    const {sheets, auth} = await fetchSheets();
-    return await sheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: range,
-        majorDimension: format
-    })
-}
-const setSheetInfo = async (format, range='InfoTEC!A3:D102',data)=>{
-    const {sheets, auth} = await fetchSheets();
-
-    return sheets.spreadsheets.values.batchUpdate({
-        spreadsheetId,
-        resource:{
-            "data": [
-                {
-                    range,
-                    "values": data,
-                    majorDimension: 
-                    format,
-                }
-            ],
-            "valueInputOption": "USER_ENTERED"
-        }
-        
-    }
-    )
-}
-
-const getUserColumns = async (user) => {
-    
-    const sheetData = await getSheetInfo('COLUMNS');
-  
-    const arrOfColumns = sheetData.data.values;
-
-    const normalizedUser = toNormalForm(user);
-    
-    const requestedUserColumn = arrOfColumns.filter((e, index) => {
-        if (!e[0]) return false;
-    
-        normalizedUserIterated = toNormalForm(e[0]);
-        const requestedUserColumn = normalizedUserIterated.includes(normalizedUser)
-        return requestedUserColumn;
-      }).flat();
-    return {requestedUserColumn, arrOfColumns}
-  };
-const toNormalForm = (str) => {
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-}
-const today = ()=>{
-    const date = new Date();
-    const day = date.getDate()
-    const month = date.getMonth() + 1;
-
-    return `${day}/${month}`
-}
-const arraysEqual = (arr1, arr2)=> {
-    // Check if the arrays have the same length
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
-  
-    // Check if every element in arr1 is equal to the corresponding element in arr2
-    return arr1.every((element, index) => {
-      return element === arr2[index];
-    });
+  "Fecha/Alumno": "A3:A",
+  "Maximo Larraya": "B3:B",
+  "Martin Delbueno": "C3:C",
+  "Oliverio Velazquez": "D3:D",
+};
+const setUserHours = async (user, hours, date = today()) => {
+  const range = "InfoTEC!A3:D102";
+  let { requestedUserColumn, arrOfColumns } = await getUserColumns(user, range);
+  const indexOfNewCell = requestedUserColumn.length;
+  const userColumnIndex = getIndexColumn(arrOfColumns, requestedUserColumn);
+  requestedUserColumn[indexOfNewCell] = hours;
+  arrOfColumns[userColumnIndex] = requestedUserColumn;
+  const dateColumns = arrOfColumns[0];
+  if (!dateColumns[indexOfNewCell]) {
+    dateColumns[indexOfNewCell] = date;
+    arrOfColumns[0] = dateColumns;
   }
-const getIndexColumn = (arrOfColumns, column)=>{
-    const indexOfColumn = arrOfColumns.findIndex(e => arraysEqual(e,column))
-    return indexOfColumn
-}
-module.exports = {fetchUserHours, setUserHours}
 
+  let res = null;
+  try {
+    res = await setSheetInfo("COLUMNS", range, arrOfColumns);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    return res.data.totalUpdatedColumns;
+  }
+};
 
+const fetchUserHours = async (user) => {
+  const { requestedUserColumn } = await getUserColumns(user);
+  const totalHours = requestedUserColumn.at(-1);
 
+  return totalHours;
+};
+
+const googleAuth = () => {
+  return new google.auth.GoogleAuth({
+    keyFile: "./credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+};
+const fetchSheets = async () => {
+  const auth = googleAuth();
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: "v4", auth: client });
+  return {
+    sheets,
+    auth,
+  };
+};
+const getSheetInfo = async (format, range) => {
+  const { sheets, auth } = await fetchSheets();
+  return await sheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: range,
+    majorDimension: format,
+  });
+};
+const setSheetInfo = async (format, range = "InfoTEC!A3:D102", data) => {
+  const { sheets, auth } = await fetchSheets();
+
+  return sheets.spreadsheets.values.batchUpdate({
+    spreadsheetId,
+    resource: {
+      data: [
+        {
+          range,
+          values: data,
+          majorDimension: format,
+        },
+      ],
+      valueInputOption: "USER_ENTERED",
+    },
+  });
+};
+
+const getUserColumns = async (user, range = "InfoTEC!A3:D103") => {
+  const sheetData = await getSheetInfo("COLUMNS", range);
+
+  const arrOfColumns = sheetData.data.values;
+
+  const normalizedUser = toNormalForm(user);
+
+  const requestedUserColumn = arrOfColumns
+    .filter((e, index) => {
+      if (!e[0]) return false;
+
+      normalizedUserIterated = toNormalForm(e[0]);
+      const requestedUserColumn =
+        normalizedUserIterated.includes(normalizedUser);
+      return requestedUserColumn;
+    })
+    .flat();
+  return { requestedUserColumn, arrOfColumns };
+};
+const toNormalForm = (str) => {
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+};
+const today = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+
+  return `${day}/${month}`;
+};
+const arraysEqual = (arr1, arr2) => {
+  // Check if the arrays have the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check if every element in arr1 is equal to the corresponding element in arr2
+  return arr1.every((element, index) => {
+    return element === arr2[index];
+  });
+};
+const getIndexColumn = (arrOfColumns, column) => {
+  const indexOfColumn = arrOfColumns.findIndex((e) => arraysEqual(e, column));
+  return indexOfColumn;
+};
+module.exports = { fetchUserHours, setUserHours };
